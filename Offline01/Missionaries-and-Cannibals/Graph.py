bf81e16f374d3f78b86f39d72b8cc1a39032c051
Subangkar-GitKraken-Win10-HP <subangkar.karmaker@gmail.com>
2018-11-17 19:44:31 +0600

    Graph Formation Removed
    -> Working

--- Offline01/Missionaries-and-Cannibals/Graph.py
+++ Offline01/Missionaries-and-Cannibals/Graph.py
@@ -14,7 +14,7 @@ class Graph:
 	def __init__(self):
 
 		# default dictionary to store graph
-		self.exploredDFS = 0
+		self.expandedDFS = 0
 		self.bfs_path = None
 		self.graph = defaultdict(list)
 		self.bfs_parent = {}
@@ -28,19 +28,14 @@ class Graph:
 		self.graph[u].append(v)
 
 	def BFS(self, s):
-		visited = defaultdict(list)
-		for u in listStates:
-			visited[u] = False
-
-		queue = [s]
-
-		visited[s] = True
 		self.bfs_parent[s] = None
-		s.level = 0
-
 		self.exploredBFS = 1
 
+		visited = {s: True}
+		s.level = 0
+
 		start_time = time.time()
+		queue = [s]
 		while queue:
 			self.expandedBFS += 1
 
@@ -48,8 +43,8 @@ class Graph:
 
 			if u.isGoalState():
 				print("No of Expanded Nodes: " + str(self.expandedBFS))
-				# print("No of Explored Nodes: " + str(visited.__len__()))
-				print("No of Explored Nodes: " + str(self.exploredBFS))
+				print("No of Explored Nodes: " + str(visited.__len__()))
+				# print("No of Explored Nodes: " + str(self.exploredBFS))
 				queue.clear()
 				self.bfs_parent[TERMINAL_STATE] = u
 				return self.bfs_parent
@@ -66,7 +61,7 @@ class Graph:
 				return []
 
 			for v in u.successors():
-				if not visited[v]:
+				if v not in visited.keys():
 					self.bfs_parent[v] = u
 					v.level = u.level + 1
 					queue.append(v)
@@ -77,38 +72,38 @@ class Graph:
 		return []
 
 	def DFS(self, start):
-		visited = defaultdict(list)
-		for u in listStates:
-			visited[u] = False
+		visited = {start: True}
+		# for u in listStates:
+		# 	visited[u] = False
 
-		visited[start] = True
 		self.dfs_parent[start] = None
 		stack = [start]
 
 		start_time = time.time()
 		while stack:
 			u = stack.pop()
+			self.expandedDFS += 1
+
 			if u.isGoalState():
-				print("No of Expanded Nodes: " + str(self.exploredDFS))
+				print("No of Expanded Nodes: " + str(self.expandedDFS))
+				print("No of Explored Nodes: " + str(visited.__len__()))
 				self.dfs_parent[TERMINAL_STATE] = u
 				stack.clear()
-				# print(self.dfs_parent)
 				return self.dfs_parent
 
 			t = time.time() - start_time
-			if t > u.CONSTANTS.MAX_TIME or self.exploredDFS > u.CONSTANTS.MAX_NODES:
+			if t > u.CONSTANTS.MAX_TIME or self.expandedDFS > u.CONSTANTS.MAX_NODES:
 				if t > u.CONSTANTS.MAX_TIME:
 					print("%.2fs EXCEEDED TIME LIMIT of %.2fs" % (t, u.CONSTANTS.MAX_TIME))
 				else:
 					print("EXCEEDED NODE LIMIT of %d" % u.CONSTANTS.MAX_NODES)
-					print("No of Expanded Nodes: " + str(self.expandedBFS))
-					print("No of Explored Nodes: " + str(self.exploredBFS))
-					stack.clear()
-					return []
+				print("No of Expanded Nodes: " + str(self.expandedDFS))
+				print("No of Explored Nodes: " + str(visited.__len__()))
+				stack.clear()
+				return []
 
 			for v in u.successors():
-				if not visited[v]:
-					self.exploredDFS += 1
+				if v not in visited.keys():
 					visited[v] = True
 					self.dfs_parent[v] = u
 					stack.append(v)
--- Offline01/Missionaries-and-Cannibals/main.py
+++ Offline01/Missionaries-and-Cannibals/main.py
@@ -6,58 +6,37 @@ import time
 from Graph import Graph
 from State import State, Direction, TERMINAL_STATE
 
-# # from State import MAX_M
-# from State import MAX_C
-# from State import CAP_BOAT
-
+CON_IN = sys.stdin
+CON_OUT = sys.stdout
 
 from Constants import CONST
-
-from Graph import listStates
-
-
-def generateAllStates(nMissionaries, nCannibals):
-	for m in range(nMissionaries + 1):
-		for c in range(nCannibals + 1):
-			for dir in [0, 1]:
-				state = State(m, c, int(not dir), 0, 0, 0, None)
-				if not state.isValid():
-					continue
-				listStates.append(state)
-
-
-def generateGraph(g):
-	for state in listStates:
-		childs = state.successors()
-		# print(state,end=" -> ")
-		# print(childs)
-		for child in childs:
-			g.addEdge(state, child)
 
 
 def runBFS(g, INITIAL_STATE):
 	sys.stdout = open("outBFS.txt", "w")
-	print(">>>>>>>>>>>\n\nBFS\n")
+	print("\n\nBFS :: \n")
 	start_time = time.time_ns()
 	p = g.BFS(INITIAL_STATE)
+	end_time = time.time_ns()
 	# print(p)
 	if len(p):
 		g.printPathL(p, TERMINAL_STATE)
 	else:
 		print("No Solution")
-	print("\n Elapsed time in BFS: %.2fms" % ((time.time_ns() - start_time) / (10 ** 6)))
+	print("\n Elapsed time in BFS: %.2fms" % ((end_time - start_time) / (10 ** 6)))
 
 
 def runDFS(g, INITIAL_STATE):
 	sys.stdout = open("outDFS.txt", "w")
-	print(">>>>>>>>>>>\n\nDFS\n")
+	print("\n\nDFS :: \n")
 	start_time = time.time_ns()
 	p = g.DFS(INITIAL_STATE)
+	end_time = time.time_ns()
 	if len(p):
 		g.printPathL(p, TERMINAL_STATE)
 	else:
 		print("No Solution")
-	print("\n Elapsed time in DFS: %.2fms" % ((time.time_ns() - start_time) / (10 ** 6)))
+	print("\n Elapsed time in DFS: %.2fms" % ((end_time - start_time) / (10 ** 6)))
 
 
 def main():
@@ -74,21 +53,22 @@ def main():
 	n = int(input("NODE_LIMIT="))
 	print(n, end="\n")
 
-	# sys.stdout = open("out.txt", "w")
-
 	CNST = CONST(m, c, k, t, n)
 
 	INITIAL_STATE = State(CNST.MAX_M, CNST.MAX_C, Direction.OLD_TO_NEW, 0, 0, 0, CNST)
 	# TERMINAL_STATE = State(-1, -1, Direction.NEW_TO_OLD, -1, -1, 0)
 
 	g = Graph()
-	generateAllStates(CNST.MAX_M, CNST.MAX_C)
-	# generateGraph(g)
-	# g.printGraph()
-
+	sys.stdout = CON_OUT
+	print("\nRunning BFS>")
 	runBFS(g, INITIAL_STATE)
+	sys.stdout = CON_OUT
+	print("Executed BFS>")
 
+	print("\nRunning DFS>")
 	runDFS(g, INITIAL_STATE)
+	sys.stdout = CON_OUT
+	print("Executed DFS>")
 
 
 if __name__ == '__main__':

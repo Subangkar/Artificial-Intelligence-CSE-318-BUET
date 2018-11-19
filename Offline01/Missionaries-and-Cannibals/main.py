09ccf4a596bd55d9585f8baf5d28b74bed7acb53
Subangkar-GitKraken-Win10-HP <subangkar.karmaker@gmail.com>
2018-11-19 11:09:08 +0600

    moves gen. in advance

--- Offline01/Missionaries-and-Cannibals/main.py
+++ Offline01/Missionaries-and-Cannibals/main.py
@@ -12,13 +12,24 @@ CON_IN = sys.stdin
 CON_OUT = sys.stdout
 
 
+def genPossibleMoves(CAP_BOAT):
+	moves = []
+	for m in range(CAP_BOAT + 1):
+		for c in range(CAP_BOAT + 1):
+			if 0 < m < c:
+				continue
+			if 1 <= m + c <= CAP_BOAT:
+				moves.append((m, c))
+	return moves
+
+
 def runBFS(g, INITIAL_STATE):
 	sys.stdout = open("outBFS.txt", "w")
 	print("\n\nBFS :: \n")
 	start_time = time.time_ns()
 	p = g.BFS(INITIAL_STATE)
 	end_time = time.time_ns()
-	# print(p)
+	# print("Printing Solution...")
 	if len(p):
 		g.printPath(p, TERMINAL_STATE)
 	else:
@@ -55,7 +66,10 @@ def main():
 
 	CNST = CONST(m, c, k, t, n)
 
-	INITIAL_STATE = State(CNST.MAX_M, CNST.MAX_C, Direction.OLD_TO_NEW, 0, 0, 0, CNST)
+	moves = genPossibleMoves(CNST.CAP_BOAT)
+	print(str(moves.__len__())+" iterations per Node.")
+
+	INITIAL_STATE = State(CNST.MAX_M, CNST.MAX_C, Direction.OLD_TO_NEW, 0, 0, 0, CNST, moves)
 	# TERMINAL_STATE = State(-1, -1, Direction.NEW_TO_OLD, -1, -1, 0)
 
 	g = Graph()
